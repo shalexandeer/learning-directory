2542cbb48d58f1703aaa10de18419132b9ad93da
shalexandeer <shalexander09@gmail.com>
2023-12-28 19:41:17 +0700

    update

--- Neetcode/ArrayAndHashing/contains-duplicate.js
+++ Neetcode/ArrayAndHashing/contains-duplicate.js
@@ -1,4 +1,4 @@
-var containsDuplicate = function(nums) {
+const  containsDuplicate = function(nums) {
   let hashset = new Set();
   for (let i = 0; i < nums.length; i++) {
     if(hashset.has(nums[i])) {
@@ -8,3 +8,16 @@ var containsDuplicate = function(nums) {
   }
   return false;
 }
+
+const RegularSort = function(nums){
+  const newArr = nums.sort((a,b) => a - b )
+  for (let i = 0; i < newArr.length; i++) {
+     if(nums[i] === nums[i-1]) return true;
+  }
+  return false;
+}
+
+
+
+console.log(RegularSort([20, 1, 3, 2, 4, 6, 8, 5, 7, 9, 11, 13, 15, 10, 12, 14, 16, 18, 17, 19]
+));
